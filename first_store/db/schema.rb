aa1b1b9cbe862758450389bbd40e1ec1e217034a
odk211 <tomoki.odaka@gmail.com>
2015-07-13 13:54:35 +0900

    rake db:migrate

--- first_store/db/schema.rb
+++ first_store/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713043857) do
+ActiveRecord::Schema.define(version: 20150713045320) do
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",           limit: 255, null: false
@@ -918,13 +918,19 @@ ActiveRecord::Schema.define(version: 20150713043857) do
     t.string   "authentication_token",   limit: 255
     t.string   "unlock_token",           limit: 255
     t.datetime "locked_at"
-    t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
     t.string   "spree_api_key",          limit: 48
+    t.datetime "remember_created_at"
+    t.datetime "deleted_at"
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "spree_users", ["deleted_at"], name: "index_spree_users_on_deleted_at", using: :btree
+  add_index "spree_users", ["email"], name: "email_idx_unique", unique: true, using: :btree
   add_index "spree_users", ["spree_api_key"], name: "index_spree_users_on_spree_api_key", using: :btree
 
   create_table "spree_variants", force: :cascade do |t|
