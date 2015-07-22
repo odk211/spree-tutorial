93532a834f1d3a2de1577e88ed969e607314bba8
odk211 <tomoki.odaka@gmail.com>
2015-07-22 16:08:29 +0900

    add URL setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def default_url_options
+    options = {}
+    if ENV['APP_HOST'].present?
+      options.merge!(host: ENV['APP_HOST'])
+    end
+    if ENV['APP_PORT'].present?
+      options.merge!(port: ENV['APP_PORT'])
+    end
+    if ENV['APP_PROTOCOL'].present?
+      options.merge!(protocol: ENV['APP_PROTOCOL'])
+    end
+    options
+  end
 end
