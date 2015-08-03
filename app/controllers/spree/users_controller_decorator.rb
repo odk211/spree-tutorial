caa03b3dc7a6468b2de008f5493939544199c947
odk211 <tomoki.odaka@gmail.com>
2015-08-03 09:19:10 +0900

    insert blank line

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -1,5 +1,7 @@
 Spree::UsersController.class_eval do
+
   private
+
   def user_params
     params.require(:user).permit(permitted_user_attributes |
                                    [ship_address_attributes: permitted_address_attributes,
--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -2,6 +2,7 @@ Spree::Address.class_eval do
   def state_text
     state.try(:name) || state.try(:abbr) || state_name
   end
+
   def full_name
     "#{lastname} #{firstname}".strip
   end
